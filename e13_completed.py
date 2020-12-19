e61ea0bd3e11b61d66e2fdfd8f512994815d5be1
Panagiotis Kriparakos <73951066+panoskrip@users.noreply.github.com>
2020-12-19 14:59:20 +0200

    Update e13_completed.py

--- e13_completed.py
+++ e13_completed.py
@@ -1,23 +1,28 @@
 fileName = str(input("Δώστε το όνομα του αρχείου: "))
+try:
+    f = open(fileName, 'r')
 
-f = open(fileName, 'r')
+    words = f.read().split()
 
-words = f.read().split()
+    # Συνάρτηση για τον εντοπισμό της θέσης των λέξεων που το άθροισμα των γραμμάτων τους είναι 20
+    def find_pair(li):
+        for i in range(len(li)-1):
+            for k in range(i, len(li)):
+                if len(li[i]+li[k])==20:
+                    return (i, k)
+        return (0,0)
 
-def find_pair(l):
-    for i in range(len(l)-1):
-        for k in range(i, len(l)):
-            if len(l[i]+l[k])==20:
-                return (i, k)
-    return (0,0)
+    while True:
+        x=find_pair(words)
+        if x!=(0,0):
+            # Εκτυπώνεται το ζευγάρι των λέξεων
+            print(words.pop(x[1]))
+            print(words.pop(x[0]))
+            print("")
+        else:
+            break
 
-while True:
-    x=find_pair(words)
-    if x!=(0,0):
-        print(words.pop(x[1]))
-        print(words.pop(x[0]))
-        print("")
-    else:
-        break
-
-print(words)
+    # Η λίστα με τις λέξεις που έχουν απομείνει:
+    print(words)
+except:
+    print("Δεν υπάρχει αρχείο με αυτό το όνομα. Σιγουρευτείτε ότι έχετε συμπεριλάβει και την κατάληξη .txt")
